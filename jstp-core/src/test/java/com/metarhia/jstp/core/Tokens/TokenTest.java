0a3e581bf6c8d2998e1c9a73bf4cd9f3dbecb9e2
lundibundi <shishugi@gmail.com>
2017-02-26 17:10:15 +0200

    add few token and parse throw test

---
+++ jstp-core/src/test/java/com/metarhia/jstp/core/Tokens/TokenTest.java
@@ -0,0 +1,40 @@
+package com.metarhia.jstp.core.Tokens;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
+/**
+ * Created by lundibundi on 2/26/17.
+ */
+public class TokenTest {
+
+  private static final List<TestData> tokenTestData = Arrays.asList(
+      new TestData('[', Token.SQ_OPEN),
+      new TestData(']', Token.SQ_CLOSE),
+      new TestData('{', Token.CURLY_OPEN),
+      new TestData('}', Token.CURLY_CLOSE),
+      new TestData(':', Token.COLON),
+      new TestData(',', Token.COMMA)
+  );
+
+  @Test
+  public void fromString() throws Exception {
+    for (TestData td : tokenTestData) {
+      assertEquals(td.expected, Token.fromString(td.input));
+    }
+  }
+
+  private static class TestData {
+
+    char input;
+    Token expected;
+
+    public TestData(char input, Token expected) {
+      this.input = input;
+      this.expected = expected;
+    }
+  }
+}
