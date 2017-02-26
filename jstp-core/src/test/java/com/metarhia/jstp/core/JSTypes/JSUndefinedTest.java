70c35c72b7b1d4e1e68a9822bb778832dfd76846
lundibundi <shishugi@gmail.com>
2017-02-26 16:49:32 +0200

    add few useless undefined tests

--- jstp-core/src/test/java/com/metarhia/jstp/core/JSTypes/JSUndefinedTest.java
+++ jstp-core/src/test/java/com/metarhia/jstp/core/JSTypes/JSUndefinedTest.java
@@ -1,10 +1,22 @@
 package com.metarhia.jstp.core.JSTypes;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class JSUndefinedTest {
+
+  @Test
+  public void isUndefined() throws Exception {
+    assertTrue(JSUndefined.isUndefined(JSUndefined.get()));
+  }
+
+  @Test
+  public void toStringTest() throws Exception {
+    assertEquals("undefined", JSUndefined.get().toString());
+  }
 
   @Test
   public void equals() throws Exception {
