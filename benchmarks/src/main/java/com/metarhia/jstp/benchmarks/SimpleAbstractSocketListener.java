6e510498a476498722b36046cb4da7bfe8969816
lundibundi <shishugi@gmail.com>
2017-03-21 23:12:09 +0200

    update project files

---
+++ benchmarks/src/main/java/com/metarhia/jstp/benchmarks/SimpleAbstractSocketListener.java
@@ -0,0 +1,30 @@
+package com.metarhia.jstp.benchmarks;
+
+import com.metarhia.jstp.connection.AbstractSocket.AbstractSocketListener;
+import com.metarhia.jstp.core.JSTypes.JSObject;
+
+/**
+ * Created by lundibundi on 3/16/17.
+ */
+public class SimpleAbstractSocketListener implements AbstractSocketListener {
+
+  @Override
+  public void onConnected() {
+
+  }
+
+  @Override
+  public void onPacketReceived(JSObject jsObject) {
+
+  }
+
+  @Override
+  public void onConnectionClosed(int i) {
+
+  }
+
+  @Override
+  public void onError(Exception e) {
+
+  }
+}
