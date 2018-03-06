a67efdcd4d95b0e2ff3485281556247b476d5ca6
Denys Otrishko <shishugi@gmail.com>
2018-03-06 13:33:45 +0200

    jstp: add simple util connect methods

--- jstp/src/main/java/com/metarhia/jstp/JSTP.java
+++ jstp/src/main/java/com/metarhia/jstp/JSTP.java
@@ -1,8 +1,12 @@
 package com.metarhia.jstp;
 
+import com.metarhia.jstp.connection.AppData;
+import com.metarhia.jstp.connection.Connection;
+import com.metarhia.jstp.connection.SimpleConnectionListener;
 import com.metarhia.jstp.core.JSParser;
 import com.metarhia.jstp.core.JSParsingException;
 import com.metarhia.jstp.core.JSSerializer;
+import com.metarhia.jstp.transport.TCPTransport;
 import java.io.Serializable;
 
 /**
@@ -25,5 +29,52 @@ public final class JSTP implements Serializable {
    */
   public static <T> String stringify(T value) {
     return JSSerializer.stringify(value);
+  }
+
+  public static void connectTCP(String app, String host, int port,
+                                final ConnectCallback callback) {
+    TCPTransport transport = new TCPTransport(host, port);
+    Connection connection = new Connection(transport);
+    connection.setAppData(AppData.valueOf(app));
+    connectTCP(connection, null, callback);
+  }
+
+  public static void connectTCP(final Connection connection,
+                                String host, int port,
+                                final ConnectCallback callback) {
+    connectTCP(connection, new TCPTransport(host, port), callback);
+  }
+
+  public static void connectTCP(final Connection connection,
+                                final TCPTransport transport,
+                                final ConnectCallback callback) {
+    if (connection.getAppName() == null) {
+      throw new IllegalArgumentException("Connection must have application data");
+    }
+    connection.addListener(new SimpleConnectionListener() {
+      @Override
+      public void onConnected(boolean restored) {
+        connection.removeListener(this);
+        callback.onConnected(connection);
+      }
+
+      @Override
+      public void onConnectionClosed() {
+        connection.removeListener(this);
+        callback.onError(null);
+      }
+    });
+    if (transport != null) {
+      connection.useTransport(transport);
+    } else {
+      connection.connect(connection.getAppData());
+    }
+  }
+
+  public interface ConnectCallback {
+
+    void onConnected(Connection connection);
+
+    void onError(Exception e);
   }
 }
