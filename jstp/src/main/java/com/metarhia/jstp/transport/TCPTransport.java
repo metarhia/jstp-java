6e12c4ff64537b427b900c9b4c10ebddb96360dd
Denys Otrishko <shishugi@gmail.com>
2018-11-06 22:55:35 +0200

    transport: report error callback before 'close'

--- jstp/src/main/java/com/metarhia/jstp/transport/TCPTransport.java
+++ jstp/src/main/java/com/metarhia/jstp/transport/TCPTransport.java
@@ -169,8 +169,8 @@ public class TCPTransport implements Transport {
         } catch (Exception e) {
           connecting = false;
           logger.info("Cannot create socket: ", e);
-          closeInternal();
           errorCallback(callback, e);
+          closeInternal();
         }
       }
     }).start();
